49b7a0aebbe6b9efafc3611ef15d33bef5e633ee
moi <machinelearning001@meras.com.es>
2025-12-08 17:10:21 +0000

    2025-12-08_002

---
+++ fitos/__init__.py
@@ -0,0 +1,4 @@
+__version__ = "0.1.0"
+
+def common_function():
+    return "Hello from Fito"
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="fitos",
+    version="0.1",
+    packages=["fitos"],
+    install_requires=[
+        'Werkzeug',
+        'setuptools',
+    ]
+)
