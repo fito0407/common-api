7af488d103b4be2d2f04ed7befe8c8e897dcbc55
moi <machinelearning001@meras.com.es>
2026-01-05 15:44:12 +0000

    save_list_of_dicts_by_index

--- fitos/fito_util.py
+++ fitos/fito_util.py
@@ -38,3 +38,11 @@ def get_date_from_utc_iso86001string(value):
 def get_elapsed_milliseconds(dt_end, dt_init):
     answer= (dt_end - dt_init).total_seconds() * 1000
     return answer
+
+def save_list_of_dicts_by_index(data_list, folder_name):
+    os.makedirs(folder_name, exist_ok=True)
+    for index, data_dict in enumerate(data_list):
+        filename = f"{index}.json"
+        filepath = os.path.join(folder_name, filename)
+        with open(filepath, 'w', encoding='utf-8') as f:
+            json.dump(data_dict, f, ensure_ascii=False, indent=4)
