445d251347d7d2633bf8b5c65280070a38bc4589
moi <machinelearning001@meras.com.es>
2026-03-06 14:30:23 +0000

    version="0.2.6"

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="fitos",
-    version="0.2.5",
+    version="0.2.6",
     packages=["fitos"],
     install_requires=[
         'Werkzeug',
