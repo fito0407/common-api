c6509d97120d5c2666df3508ed82d07e4def85bb
moi <machinelearning001@meras.com.es>
2026-03-06 13:36:46 +0000

    ari_clusterings first validation

--- fitos/fito_ml.py
+++ fitos/fito_ml.py
@@ -1,4 +1,5 @@
 from sklearn import metrics
+import pandas as pd
 
 def clustering_to_labels(clustering, element_order):
     element_to_cluster = {}
@@ -11,8 +12,18 @@ def clustering_to_labels(clustering, element_order):
 
     return answer
 
-def ari_clusterings(clustering1, clustering2, element_order):
-    labels1 = clustering_to_labels(clustering1, element_order)
-    labels2 = clustering_to_labels(clustering2, element_order)
-    answer = metrics.adjusted_rand_score(labels1, labels2)
+def ari_clusterings(clustering_true, clustering_pred, element_order):
+
+    grouped_elements = (pd.DataFrame({'element': element_order})
+                        .groupby('element')
+                        .agg(number=('element', 'count'))
+                        .reset_index())
+    clusters_with_multiple = len(grouped_elements[grouped_elements['number'] > 1])
+    if clusters_with_multiple >0:
+        duplicates = grouped_elements[grouped_elements['number'] > 1]['element'].tolist()
+        raise ValueError(f'element_order cannot have repeated elements. Duplicates found: {duplicates}')
+
+    labels_true = clustering_to_labels(clustering_true, element_order)
+    labels_pred = clustering_to_labels(clustering_pred, element_order)
+    answer = metrics.adjusted_rand_score(labels_true= labels_true, labels_pred= labels_pred)
     return answer
--- test.py
+++ test.py
@@ -1,14 +1,16 @@
 import fitos.fito_util as fu
 import fitos.fito_ml as fm
 
-print(fu.get_now_utc())
-print(fu.get_now_utc_x_days_back(30))
+#print(fu.get_now_utc())
+#print(fu.get_now_utc_x_days_back(30))
 
 
 #df_files= fu.get_dataframe_from_jsons('test_json')
 #a=df_files
 
 element_order = ['A', 'B', 'C', 'D', 'E', 'F']
+wrong_element_order = ['A', 'B', 'C', 'D', 'E', 'F', 'F']
+
 clustering1 = [['A', 'B', 'C'], ['D'], ['E', 'F']]
 clustering2 = [['A', 'B'], ['C', 'D'], ['E', 'F']]
 ari= fm.ari_clusterings(clustering1, clustering2, element_order)
